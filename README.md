9119d40931d7052e3742b21a9f13b994e9f28280
Andrew GIBSON <andrewgib14@gmail.com>
2017-07-28 10:05:28 -0500

    made corrections

---
+++ README.md
@@ -0,0 +1,42 @@
+The average price is $23.63
+
+1970s Coors Banquet Glass Beer Pitcher
+1970s Schlitz Malt Liquor Glass Beer Pitcher
+The Three Broomsticks Customizable Beer Stein Mug, Harry Potter Inspired, hogsmeade village, harry potter gift, three broomsticks mug
+Hand Painted Colorful Feather Glass
+Groomsman Gifts - Bottle Opener Credit Card Sized PERSONALIZED - FREE SHIPPING
+
+1970s Schlitz Malt Liquor Glass Beer Pitcher costs 18 pounds.
+
+SALE Mid Century Siesta Ware White Mug with Anchor - Set of 3 is made of wood.
+Bottle cap catcher personalized. Man cave gift for him- Wooden Beer pub sign - Groomsmen wedding Gift is made of wood.
+Medium Size, Welcome To Our Firepit-Where Friends And Marshmallows Get Toasted At The Same Time-Painted Wood Sign-Custom Colors is made of wood.
+Magnetic Wall Mount Bottle Opener Barware Set - Stainless Steel or Black - Personalized if you like! is made of wood.
+Engraved Pocket Knife, Personalized Groomsmen Gift, Ring Bearer Gift, Graduation Gift, 4 Knives is made of wood.
+
+Qty of 2 Groomsmen Gift - Stainless Steel Personalized Bottle Opener - NO Capcatcher has 9 materials:
+- wall mount bottle opener
+- wedding
+- man cave
+- christmas gift
+- guy gift
+- fathers day
+- home bar
+- beer
+- bar
+The Three Broomsticks Customizable Beer Stein Mug, Harry Potter Inspired, hogsmeade village, harry potter gift, three broomsticks mug has 13 materials:
+- glass
+- sandblast cabinet
+- vinyl
+- beer glass
+- pint glass
+- etched pint glass
+- etched glass
+- etched beer glass
+- 16 oz pint
+- beer gift
+- etched harry potter glass
+- the three broomsticks glass
+- personalized harry potter glass
+
+18 items were made by their sellers.
--- main.js
+++ main.js
@@ -7,40 +7,89 @@ console.log(data);
 
 
 // 1: Show me how to calculate the average price of all items.
-function question1 () {
+function question1() {
   // Answer:
+  let total_price = 0;
+  for (let i = 0; i < data.length; i++) {
+    total_price += data[i].price;
+  }
+  console.log(total_price / data.length);
+  return (total_price / data.length);
 }
 
 
 
 // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
+function question2() {
   // Answer:
+  let myArray = [];
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].price <= 18 && data[i].price >= 14) {
+      myArray.push(data[i]);
+    }
+  }
+  console.log(myArray);
+  return myArray;
 }
 
 
 
 // 3: Which item has a "GBP" currency code? Display it's name and price.
-function question3 () {
+function question3() {
   // Answer:
+  let gBP_code;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].currency_code === "GBP") {
+      gBP_code = data[i];
+    }
+  }
+  console.log(gBP_code.title);
+  console.log(gBP_code.price);
+  return gBP_code;
 }
 
 
 // 4: Display a list of all items who are made of wood.
-function question4 () {
+function question4() {
   // Answer:
+  let woodArray = [];
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].materials.includes("wood")) {
+      woodArray.push(data[i]);
+    }
+  }
+  console.log(woodArray);
+  return woodArray;
 }
+
+
 
 
 // 5: Which items are made of eight or more materials? 
 //    Display the name, number of items and the items it is made of.
-function question5 () {
+function question5() {
   // Answer:
+  let lrgItemArray
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].materials.length >= 8) {
+      console.log(data[i].title);
+      console.log(data[i].materials.length)
+      console.log(data[i].materials);
+    }
+  }
 }
 
 
 // 6: How many items were made by their sellers?
 // Answer:
-function question6 () {
+function question6() {
   // Answer:
+  let itemCount = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].who_made === "i_did") {
+      itemCount++
+    }
+  }
+  console.log(itemCount + " items were made by there sellers.");
 }
